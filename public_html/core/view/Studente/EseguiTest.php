f6dbf98ce5b16377a6d737e1240361542b0e503d
Fabiano <Fabiano@Fabiano.lan>
2015-11-21 16:38:58 +0100

    Modificate le classi "Seleziona domande" (docente) e "Esegui Test" (studente)
    Aggiunti "logoPiccolo.png" "logoMedio.png" "logoGrande.png" in assets -> admin -> layout -> img
    
    Modificata URL logo nella classe "headMenu"

--- public_html/core/view/Studente/EseguiTest.php
+++ public_html/core/view/Studente/EseguiTest.php
@@ -37,39 +37,96 @@ $controller = new Esempio();
     <div class="page-content-wrapper">
         <div class="page-content">
             <!-- BEGIN PAGE HEADER-->
-
+                <h3 class="page-title">
+                    Visualizza Test
+                </h3>
+                <div class="page-bar">
+                    <ul class="page-breadcrumb">
+                        <li>
+                            <i class="fa fa-home"></i>
+                            <a href="index.html">Home</a>
+                            <i class="fa fa-angle-right"></i>
+                        </li>
+                        <li>
+                            <a href="#">Esegui Test</a>
+                            <i class="fa fa-angle-right"></i>
+                        </li>
+                    </ul>
+                </div>
             <!-- END PAGE HEADER-->
             <!-- BEGIN PAGE CONTENT-->
-            <form>
-                <fieldset>
-                    <legend> Test </legend>
-                    <h3> Domanda 1 (multipla) </h3>
-                    <p><input type = "checkbox"/>Risposta 1</p>
-                    <p><input type = "checkbox"/>Risposta 2</p>
-                    <p><input type = "checkbox"/>Risposta 3</p>
-                    <p><input type = "checkbox"/>Risposta 4</p>
-                    
-                    <h3> Domanda 2 (aperta)</h3>
-                    <div class="form-group">
-                        <textarea class="form-control" rows="3" placeholder="Inserisci risposta" style="resize:none"></textarea>
+            
+            <div class="form-body">
+                <div class="portlet box grey-cascade">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-globe"></i>Test
+                        </div>
+                        <div class="tools">
+                            <div class="caption">
+                                <font size="3">Tempo rimasto: ???</font>
+                            </div>
+                        </div>
                     </div>
-                    <h3> Domanda 3 (multipla)</h3>
-                    <p><input type = "checkbox"/>Risposta 1</p>
-                    <p><input type = "checkbox"/>Risposta 2</p>
-                    <p><input type = "checkbox"/>Risposta 3</p>
-                    <p><input type = "checkbox"/>Risposta 4</p>
-                
-           
-             <div class="row">
-                <div class="col-md-12">
-                    <div class="col-md-9"></div>
-                    <button type="button" class="btn green">Abbandona</button> 
-                    <button type="submit" class="btn green">Conegna</button>  
+
+                    <div class="portlet-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group form-md-line-input has-success">
+                                    <div class="input-icon">
+                                        <input type="text" class="form-control">
+                                            <label for="form_control_1">Nome</label>
+                                                <span class="help-block">Inserire il nome</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group form-md-line-input has-success">
+                                    <div class="input-icon">
+                                        <input type="text" class="form-control">
+                                            <label for="form_control_1">Cognome</label>
+                                                <span class="help-block">Inserire il cognome</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group form-md-line-input has-success">
+                                    <div class="input-icon">
+                                        <input type="text" class="form-control">
+                                            <label for="form_control_1">Matricola</label>
+                                                <span class="help-block">Inserire la matricola</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3> Domanda 1 (multipla) </h3>
+                        <p><input type = "checkbox"/>Risposta 1</p>
+                        <p><input type = "checkbox"/>Risposta 2</p>
+                        <p><input type = "checkbox"/>Risposta 3</p>
+                        <p><input type = "checkbox"/>Risposta 4</p>
+
+                        <h3> Domanda 2 (aperta)</h3>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3" placeholder="Inserisci risposta" style="resize:none"></textarea>
+                        </div>
+                        <h3> Domanda 3 (multipla)</h3>
+                        <p><input type = "checkbox"/>Risposta 1</p>
+                        <p><input type = "checkbox"/>Risposta 2</p>
+                        <p><input type = "checkbox"/>Risposta 3</p>
+                        <p><input type = "checkbox"/>Risposta 4</p>
+
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="col-md-9"></div>
+                            <button type="button" class="btn green">Abbandona</button> 
+                            <button type="submit" class="btn green">Conegna</button>  
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
-                    </fieldset>
-            </form>
-            <!-- END PAGE CONTENT-->
+<!-- END PAGE CONTENT-->
         </div>
     </div>
     <!-- END CONTENT -->
