2a989030c36c0cdfacea742623e0a3f5c7034ac6
Giusy <Giusy@172.19.48.189>
2015-11-23 15:33:49 +0100

    Modifica SessioneModel

--- public_html/core/model/SessioneModel.php
+++ public_html/core/model/SessioneModel.php
@@ -21,87 +21,127 @@ class SessioneModel extends Model {
             . "s.sessione_id='%d' AND s.test_id= t.id";
     private static $GET_ALL_STUDENTI_SESSIONE = "SELECT * FROM `abilitazione` as a, `utente` as u WHERE "
             . "a.sessione_id='%s' AND a.studente_matricola=u.matricola";
+    
     /**
      * Inserisce una nuova sessione nel database
-     * @param Sessione sessione la sessione da inserire nel database
+     * @param Sessione $sessione La sessione da inserire nel database
      */
     public function createSessione($sessione) {
         $query = sprintf(self::$CREATE_SESSIONE, $sessione->getId, $sessione->getDataInizio, $sessione->getDataFine, $sessione->getSogliaAmmissione, $sessione->getTipologia, $sessione->getInsegnamentoId, $sessione->getInsegnamentoCorsoMatricola);
         $res = Model::getDB()->query($query);
+        if ($res) {
+            //messaggio ok
+        }
+        else{
+            //messaggio errore  
+        }
     }
 
     /**
      * Modifica una sessione nel database
-     * @param integer id L'id della sessione da modificare
-     * @param Sessione updatedSessione La sessione modificata da aggiornare nel db
+     * @param int $id L'id della sessione da modificare
+     * @param Sessione $updatedSessione La sessione modificata da aggiornare nel db
      */
     public function updateSessione($id, $updatedSessione) {
         $query = sprintf(self::$UPDATE_SESSIONE, $updatedSessione->getDataInizio, $updatedSessione->getDataFine, $updatedSessione->getSogliaAmmissione, $updatedSessione->getTipologia, $updatedSessione->getInsegnamentoId, $updatedSessione->getInsegnamentoCorsoMatricola,  $id);
         $res = Model::getDB()->query($query);
+        if ($res) {
+            //messaggio ok
+        }
+        else{
+            //messaggio errore  
+        }
     }
 
     /**
      * Cancella una sessione nel database
-     * @param integer id L'id della sessione da eliminare
+     * @param int $id L'id della sessione da eliminare
      */
     public function deleteSessione($id) {
         $query = sprintf(self::$DELETE_SESSIONE, $id);
         $res = Model::getDB()->query($query);
+        if ($res) {
+            //messaggio ok
+        }
+        else{
+            //messaggio errore  
+        }
     }
 
     /**
-     * Cerca un corso di laurea nel database
-     * @param string matricola La matricola del corso di laurea da cercare
+     * Cerca una sessione nel database
+     * @param int $id L'id della sessione da cercare
      */
     public function readSessione($id) {
         $query = sprintf(self::$READ_SESSIONE, $id);
         $res = Model::getDB()->query($query);
-        if ($res) {
-            $sessione = new Sessione($res->fetch_assoc()['id'], $res->fetch_assoc()['dataInizio'], $res->fetch_assoc()['dataFine'],  $res->fetch_assoc()['sogliaAmmissione'],  $res->fetch_assoc()['tipologia'],  $res->fetch_assoc()['insegnamentoId'],  $res->fetch_assoc()['insegnamentoCorsoMatricola']);
+        if ($obj = $res->fetch_assoc()) {
+            $sessione = new Sessione($obj['id'], $obj['dataInizio'], $obj['dataFine'],  $obj['sogliaAmmissione'],  $obj()['tipologia'],  $obj()['insegnamentoId'],  $obj['insegnamentoCorsoMatricola']);
             return $sessione;
+        }
+        else{
+            //nessuna sessione trovata
         }
     }
 
     /**
      * Restituisce tutte le sessioni del database
-     * @return Sessione[] sessioni Tutte le sessioni del database
+     * @return Sessione[] Tutte le sessioni del database
      */
     public function getAllSessioni() {
         $res = Model::getDB()->query(self::$GET_ALL_SESSIONI);
         $sessioni = array();
-        while ($obj = $res->fetch_assoc()) {
-            $sessioni[] = new Sessione($obj['id'], $obj['dataInizio'], $obj['dataFine'], $obj['sogliaAmmissione'], $obj['tipologia'], $obj['insegnamentoId'], $obj['insegnamentoCorsoMatricola']);
+        if($res){
+            while ($obj = $res->fetch_assoc()) {
+                $sessioni[] = new Sessione($obj['id'], $obj['dataInizio'], $obj['dataFine'], $obj['sogliaAmmissione'], $obj['tipologia'], $obj['insegnamentoId'], $obj['insegnamentoCorsoMatricola']);
+            }
+            return $sessioni;
         }
-        return $sessioni;
+        else{
+            //nessuna sessione trovata
+        }
     }
-      /**
+     
+    /**
      * Restituisce tutti i test di una sessione
-     * @param int $id L'id di tutti i test di una sessione
-     * @return TestSessione[] Tutti i test della sessione
+     * @param int $id L'id della sessione per la qule si vogliono cercare i test
+     * @return Test[] Tutti i test della sessione
      */
     public function getAllTestSessione($id) {
         $query = sprintf(self::$GET_ALL_TEST_SESSIONE, $id);
         $res = Model::getDB()->query($query);
         $sessione = array();
-        while ($obj = $res->fetch_assoc()) {
-            $TestSessione = new TestSessione(); //da completare
-            $sessione[]= TestSessione;
+        if($res){
+            while ($obj = $res->fetch_assoc()) {
+                $TestSessione = new Test(); //da completare
+                $sessione[]= TestSessione;
+            }
+            return $sessione;
         }
-        return $sessione;
+        else{
+            //nessun test trovato
+        }
+        
     }
-      /**
+     
+    /**
      * Restituisce tutti gli studenti che hanno partecipato ad una sessione
-     * @param int $id L'id della sessione
-     * @return StudentiSessione[] Tutti gli studenti che hanno partecipato alla sessione
+     * @param int $id L'id della sessione per la quale si vogliono conoscere gli studenti abilitati
+     * @return Studente[] Tutti gli studenti che sono abilitati alla sessione
      */
      public function getAllStudentiSessione($id) {
         $query = sprintf(self::$GET_ALL_STUDENTI_SESSIONE, $id);
         $res = Model::getDB()->query($query);
-        $sessione = array();
-        while ($obj = $res->fetch_assoc()) {
-            $StudentiSessione = new StudentiSessione(); //da completare
-            $sessione[]= StudentiSessione;
+        $studenti = array();
+        if($res){
+            while ($obj = $res->fetch_assoc()) {
+                $StudenteSessione = new Studente(); //da completare
+                $studenti[]= StudenteSessione;
+            }
+            return $studenti;
         }
-        return $sessione;
+        else{
+            //nessuno studente trovato
+        }
     }
 }
