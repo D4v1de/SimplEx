470dadb3bc79ded94b1b0f08e0f6af0329baca84
Alina <adolcina@gmail.com>
2015-11-25 14:19:20 +0100

    Modificato Error.php

--- public_html/core/utils/Error.php
+++ public_html/core/utils/Error.php
@@ -11,6 +11,12 @@ class Error {
     public static $RISPOSTA_NON_TROVATA = "Nessuna risposta trovata";
     public static $NO_APERTE = "L'elaborato non ha risposte aperte";
     public static $INSERIMENTO_FALLITO = "Impossibile inserire nel database";
+    public static $ARGOMENTO_NON_TROVATO = "Nessun argomento trovato";
+    public static $DOMANDA_APERTA_NON_TROVATA = "Nessuna domanda aperta trovata";
+    public static $DOMANDA_MULTIPLA_NON_TROVATA = "Nessuna domanda mutlipla trovata";
+    public static $ALTERNATIVA_NON_TROVATA = "Nessuna alternativa trovata";
+    public static $AGGIORNAMENTO_FALLITO = "Impossibile aggiornare il database";
+    public static $CANCELLAZIONE_FALLITA = "Impossibile cancellare dal database";
     
 }
 ?>
