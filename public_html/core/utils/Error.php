414fb908d8aee693b62b4a3fdd1697450be90b5d
dariocast <dariocastellano18@gmail.com>
2015-11-25 09:50:32 +0100

    Aggiunte eccezioni

---
+++ public_html/core/utils/Error.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * User: Dario
+ * Date: 24/11/15
+ * Time: 19:38
+ */
+
+class Error {
+    public static $ELABORATO_NON_TROVATO = "Nessun elaborato trovato";
+    public static $RISPOSTA_NON_TROVATA = "Nessuna risposta trovata";
+    public static $NO_APERTE = "L'elaborato non ha risposte aperte";
+    public static $INSERIMENTO_FALLITO = "Impossibile inserire nel database";
+    
+}
+?>
