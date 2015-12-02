ce9792e445e83cb8db4404c1ccb58f3e0d4c8025
Alina <adolcina@gmail.com>
2015-12-02 10:43:10 +0100

    Modificato AccountModel

--- public_html/core/model/AccountModel.php
+++ public_html/core/model/AccountModel.php
@@ -20,7 +20,7 @@ class AccountModel extends Model {
     // Aggiunti da Elvira
     private static $GET_ALL_DOCENTI_CORSO = "SELECT u.* FROM `insegnamento` as i, `utente` as u WHERE `i.docente_matricola` = `u.matricola` AND `i.corso_id` = '%d'";
     private static $GET_ALL_STUDENTI_CDL = "SELECT u.* FROM `utente` as u WHERE `u.cdl_matricola` = '%s'";
-    private static $GET_ALL_STUDENTI_CORSO = "SELECT u.* FROM `utente` as u, `frequenta` as f WHERE `f.studente_matricola` = `u.matricola` AND `f.corso_matricola` = '%d'";
+    private static $GET_ALL_STUDENTI_CORSO = "SELECT u.* FROM `utente` as u, `frequenta` as f WHERE f.studente_matricola = u.matricola AND f.corso_id = '%d'";
     private static $GET_ALL_STUDENTI_SESSIONE = "SELECT u.* FROM `abilitazione` as a, `utente` as u WHERE `a.sessione_id` = '%s' AND `a.studente_matricola` = `u.matricola`";
 
     // Aggiunto da Federico
@@ -113,7 +113,7 @@ class AccountModel extends Model {
      * @return Utente
      * @throws ApplicationException [$UTENTE_NON_TROVATO]
      */
-    private function parseUtente(&$res) {
+    private function parseUtente($res) {
         if ($obj = $res->fetch_assoc()) {
             return new Utente($obj['matricola'], $obj['username'], $obj['password'], $obj['tipologia'], $obj['nome'], $obj['cognome'], $obj['cdl_matricola']);
         } else {
