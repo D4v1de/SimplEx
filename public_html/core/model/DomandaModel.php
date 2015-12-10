96e7bf293b131e211142c53ea9c9dffea9bbbda7
Elvira <zaninelvira@gmail.com>
2015-12-10 16:58:10 +0100

    -Correzione di un errore in una query

--- public_html/core/model/DomandaModel.php
+++ public_html/core/model/DomandaModel.php
@@ -26,7 +26,7 @@ class DomandaModel extends Model {
     private static $READ_DOMANDA_MULTIPLA = "SELECT * FROM `domanda_multipla` WHERE id = '%d'";
     private static $GET_ALL_DOMANDA_MULTIPLA = "SELECT * FROM `domanda_multipla` WHERE stato = 'In uso' ORDER BY testo";
     private static $GET_ALL_DOMANDA_MULTIPLA_BY_ARGOMENTO = "SELECT * FROM `domanda_multipla` WHERE argomento_id = '%d' AND stato = 'In uso' ORDER BY testo";
-    private static $GET_ALL_DOMANDE_MULTIPLE_TEST = "SELECT d.* FROM `domanda_multipla` as d,`compone_multipla` as c WHERE c.test_id = '%s' AND c.domanda_multipla_id = d.id ORDER BY d./* testo";
+    private static $GET_ALL_DOMANDE_MULTIPLE_TEST = "SELECT d.* FROM `domanda_multipla` as d,`compone_multipla` as c WHERE c.test_id = '%s' AND c.domanda_multipla_id = d.id ORDER BY d.testo";
     private static $ASSOCIA_DOMANDA_APERTA_TEST = "INSERT INTO `compone_aperta` (domanda_aperta_id, test_id, punteggio_max_alternativo) VALUES ('%d','%d', '%f')";
     private static $MODIFICA_DOMANDA_APERTA_TEST = "UPDATE `compone_aperta` SET punteggio_max_alternativo = '%f' WHERE domanda_aperta_id = '%d' AND test_id= '%d'";
     private static $DISSOCIA_DOMANDA_APERTA_TEST = "DELETE FROM `compone_aperta` WHERE domanda_aperta_id = '%d' AND test_id= '%d'";
