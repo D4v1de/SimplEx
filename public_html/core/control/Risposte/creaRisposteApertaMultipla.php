e439c16481fecfa9449ab9a94dd017d1837ee253
Fabiano <Fabiano@Fabiano.lan>
2016-01-02 15:11:11 +0100

    - Aggiunti commenti alle classi

--- public_html/core/control/Risposte/creaRisposteApertaMultipla.php
+++ public_html/core/control/Risposte/creaRisposteApertaMultipla.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Created by NetBeans.
- * User: Fabiano
- * Date: 03/12/15
- * Time: 16:00
+ * Controller per la creazione delle risposte
+ *
+ * @author Fabiano Pecorelli
+ * @version 1.0
+ * @since 03/12/15
  */
 include_once MODEL_DIR . "RispostaMultiplaModel.php";
 include_once MODEL_DIR . "RispostaApertaModel.php";
