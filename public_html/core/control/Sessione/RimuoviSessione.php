1a7d82972487adcff242044db40c2497b5201c16
AntonioLuca <antonioldavanzo93@gmail.com>
2016-01-03 17:29:47 +0100

    RIriagguinte classi cancellate .-.

--- public_html/core/control/Sessione/RimuoviSessione.php
+++ public_html/core/control/Sessione/RimuoviSessione.php
@@ -5,17 +5,14 @@
  * @version 1
  * @since  30/12/2015 11:26
  */
-
 include_once MODEL_DIR . "SessioneModel.php";
 include_once MODEL_DIR . "UtenteModel.php";
 include_once MODEL_DIR . "TestModel.php";
 include_once BEAN_DIR . "Sessione.php";
-
 $sessioneModel = new SessioneModel();
 $utenteModel = new UtenteModel();
 $testModel = new TestModel();
 $idCorso = $_URL[2];
-
 if(isset($_POST['IdSes'])) {
     $idSes = $_POST['IdSes'];
     try {
@@ -25,7 +22,6 @@ if(isset($_POST['IdSes'])) {
         echo "ERRORE" . $ex;
     }
 }
-
 else {
     $idSes = $_URL[4];
     try {
